be81d9c8017c3576dca5fe7e5529c80908c0259f
Raby sene <Rabysene17@gmail.com>
2024-11-24 20:34:48 +0000

    Mise à jour des vues Admin et Reporting

--- resources/views/Admin/enregConcess.blade.php
+++ resources/views/Admin/enregConcess.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+    <h2 class="font-semibold text-xl text-gray-800 leading-tight" align="center">
             {{ __('Enregistrement des concessionaires') }}
         </h2>
     </x-slot>
